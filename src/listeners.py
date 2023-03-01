ae9f8844b9bf55b0eeec0d318ba7adb7b3895fed
mfvera <0matteov@gmail.com>
2023-02-28 20:30:14 -0700

    Fix file mode properly, add minor refactors

--- src/listeners.py
+++ src/listeners.py
@@ -13,21 +13,22 @@ async def listen_buttons(keypad, callback):
     }
     button_names = list(button_states.keys())
 
-    def is_button_pressed(name):
-        value = getattr(keypad, name)
-        if value != button_states[name]:
-            button_states[name] = value
-            return value
+    def is_button_pressed(button_name):
+        current_button_state = getattr(keypad, button_name)
+        if current_button_state != button_states[button_name]:
+            button_states[button_name] = current_button_state
+            return current_button_state
         return False
 
     while True:
         # Only get the 'first' pressed button.
-        pressed_button = next((name for name in button_names if is_button_pressed(name)), None)
+        pressed_button = next((button for button in button_names if is_button_pressed(button)), None)
         if (pressed_button):
             callback(pressed_button)
         await aio.sleep(0.1)
 
 async def listen_keyboard(callback):
     while True:
-        value = await aioconsole.ainput("Message:")
+        # Prompt is unnecessary for headless usage, but I like it for debugging. 
+        value = await aioconsole.ainput("Badge: ")
         callback(value)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import asyncio as aio
 import os
 import time
-from collections.abc import Set
+from collections.abc import MutableSet
 from datetime import date
 from io import SEEK_END, TextIOWrapper
 
@@ -12,14 +12,12 @@ from screen import LCD_COLUMNS, open_screen
 from ui import Frame, FrameController
 
 """
+TODO:
 1. Add the ability to export the user list at-will via developer provided callback
 """
 
 def centered(text: str) -> str:
     return text.center(LCD_COLUMNS)
-
-script_location = os.path.dirname(__file__)
-badge_filename = os.path.join(script_location, f"../outputs/attendance_{date.today().isoformat()}")
 
 class PiBadger(FrameController):
     def __init__(self,
@@ -30,7 +28,7 @@ class PiBadger(FrameController):
 
         self.loop = event_loop
         self.loop.create_task(listeners.listen_buttons(screen, self.advance))
-        self.loop.create_task(listeners.listen_keyboard(self.handle_badge_input))
+        self.loop.create_task(listeners.listen_keyboard(self._handle_badge_input))
 
         self.screen = screen
         self.screen.cursor = True
@@ -47,15 +45,18 @@ class PiBadger(FrameController):
         self.render()
         self.loop.run_forever()
 
-    def handle_badge_input(self, badge_id):
+    def _handle_badge_input(self, badge_id) -> None:
         # Ensure that we show the "welcome" message every time a user badges
         self.restart()
         self.render(badge_id)
     
         if badge_id not in self.badges:
+            self.badges.add(badge_id)
             self.login_count += 1
-        self.file.write(f"{badge_id}\n")
-        self.file.flush()
+
+            self.file.write(f"{badge_id}\n")
+            # Ensure the file is written in case of power loss or other interruption.
+            self.file.flush()
 
     def _build_frames(self) -> "Frame":
         @Frame
@@ -85,8 +86,6 @@ class PiBadger(FrameController):
         @Frame
         def final_shutdown_frame():
             self.screen.clear()
-            self.screen.display = False
-            self.screen.color = [0,0,0]
             self.loop.stop()
 
         main_frame.add_neighbor("right_button", total_attendees_frame)
@@ -103,17 +102,29 @@ class PiBadger(FrameController):
 
         return main_frame
 
-    def _read_existing_attendees(self) -> Set[str]:
+    def _read_existing_attendees(self) -> MutableSet[str]:
         badge_ids = set(line.strip() for line in self.file)
-        # Ensure the file is ready to append names by placing cursor at the end.
+        # I'm unsure if this is absolutely necessary due to iterating the file already, but I didn't find a satisfying SO post to prove it.
         self.file.seek(0, SEEK_END)
 
         return badge_ids
 
-with open_screen() as screen, open(badge_filename, mode="w+") as badge_file:
+"""
+Why "a+"? Because file modes in python are a tad goofy imo.
+* w+ will create the file and allow us to read and write to it, however it will 'truncate'/delete the contents of the file if it already exists. Not great.
+* r+ will allow reads and writes, however it will not allow creation of the file. Also not ideal.
+* a+ gives us reading, writing, *and* creation, however it places the seek postion at the end of the file. Certainly not perfect, but we can work around this.
+
+One more alternative would involve opening the file twice, once with a/a+ to ensure existence of the file, and then again with r+ to perform reads/writes.
+I chose to open the file only once since this alternative method feels a smidgen wonky to me.
+"""
+script_location = os.path.dirname(__file__)
+badge_filename = os.path.join(script_location, f"../outputs/attendance_{date.today().isoformat()}")
+with open_screen() as screen, open(badge_filename, mode="a+") as badge_file:
+    badge_file.seek(0)
     loop = aio.get_event_loop()
     badger = PiBadger(badge_file, loop, screen)
-    # Shutdown is handled on the "shutdown" frame renderer
+    # This will block until the final shutdown frame is rendered, at which point we resume here.
     badger.start()
 
 os.system("sudo shutdown now -P")
